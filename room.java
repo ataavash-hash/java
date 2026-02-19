ce22b0e984b6d3c1be8faf390e76505da43ef74d
Aawaj Tamang <ataavash@gmail.com>
2026-02-19 17:53:49 +0545

    Add files via upload

---
+++ room.java
@@ -0,0 +1,12 @@
+package OOPjava.sem1.OOPclass;
+
+public class room {
+        double length;
+        double breath;
+
+        void calculatearea(){
+            System.out.println("the length of room is "+ length);
+            System.out.println("the breath of the room is " + breath);
+            System.out.println(("sp the total areaa of room is "+ length*breath));
+        }
+    }
---
+++ room2.java
@@ -0,0 +1,15 @@
+package OOPjava.sem1.OOPclass;
+
+public class room2 {
+    public static void main(String[] args){
+        room s201=new room();
+        s201.length=45;
+        s201.breath=70;
+        s201.calculatearea();
+
+        room s202=new room();
+        s202.length=50;
+        s202.breath=55;
+        s202.calculatearea();
+    }
+}
