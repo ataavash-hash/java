2f6421335cdc52446776bbbc09a742eb6457493d
Aawaj Tamang <ataavash@gmail.com>
2026-02-19 17:53:19 +0545

    Add files via upload

---
+++ result2.java
@@ -0,0 +1,9 @@
+package OOPjava.sem1.OOPclass;
+
+public class result2 {
+    public static void main(String[] args){
+        result r1= new result();
+        System.out.println(r1.total(100,200,30));
+        System.out.println(r1.passed(330));
+    }
+}
