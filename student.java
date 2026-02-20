66e5b6c4096c00f02a99b796fcd8edade17e0765
Aawaj Tamang <ataavash@gmail.com>
2026-02-20 20:48:37 +0545

    Add files via upload

---
+++ student.java
@@ -0,0 +1,13 @@
+package OOPjava.sem1.OOPclass;
+
+public class student {
+        String name;
+        int rollNo;
+        int marks;
+
+        void displaydetails() {
+            System.out.println("name:" + name);
+            System.out.println("rollNo:" + rollNo);
+            System.out.println("marks:" + marks);
+        }
+    }
