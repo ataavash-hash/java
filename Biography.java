b042f58f7fa67e3cff755def30f9c0b28e128389
Aawaj Tamang <ataavash@gmail.com>
2026-02-20 20:49:57 +0545

    Add files via upload

---
+++ Biography.java
@@ -0,0 +1,11 @@
+package OOPjava.sem1.Lab1;
+
+public class Biography {
+    public static void main(String[] args) {
+            String name = "Aawaj Tamang";
+            int age = 18;
+            double height = 1.8;
+            boolean isStudent = true;
+            System.out.println("My name is " + name + ", I am " + age + " years old, " + height + "m tall, and it is " + isStudent + " that I am a student.");
+    }
+}
