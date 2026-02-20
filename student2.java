5c8d929289bed42aefca22c3ea900f7a5acc89ad
Aawaj Tamang <ataavash@gmail.com>
2026-02-20 20:48:58 +0545

    Add files via upload

---
+++ student2.java
@@ -0,0 +1,17 @@
+package OOPjava.sem1.OOPclass;
+
+public class student2{
+    public static void main(String[] args){
+        student s1 = new student();
+        s1.name = "ram";
+        s1.rollNo = 11;
+        s1.marks =60;
+        s1.displaydetails();
+
+        student s2 = new student();
+        s2.name = "sita";
+        s2.rollNo = 12;
+        s2.marks = 34;
+        s2.displaydetails();
+    }
+}
