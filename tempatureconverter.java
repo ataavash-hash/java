ca391aa490fd1fb7b41f51994d2cd5aa4142266c
Aawaj Tamang <ataavash@gmail.com>
2026-02-21 16:38:04 +0545

    Add files via upload

---
+++ tempatureconverter.java
@@ -0,0 +1,10 @@
+package OOPjava.sem1.lab2;
+
+ public class tempatureconverter {
+        public static void main(String[] args) {
+            double celsius = 20;
+            double fahrenheit= (celsius * 9.0 / 5.0) + 32;
+            System.out.println("Temperature in F: " + fahrenheit);
+        }
+}
+
