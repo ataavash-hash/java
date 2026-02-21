31358a669d7b383de1e6e7e9d4c1548eb7325197
Aawaj Tamang <ataavash@gmail.com>
2026-02-21 16:36:36 +0545

    Add files via upload

---
+++ calculatecircle.java
@@ -0,0 +1,10 @@
+package OOPjava.sem1.lab2;
+
+public class calculatecircle {
+        public static void main(String[] args) {
+            double pie_value = 3.141659;
+            double radius = 7.5;
+            double area = pie_value * radius * radius;
+            System.out.println("Area of the circle: " + area);
+        }
+}
