9c4c92ab6b8144cecab2f5a2a8cb4d0a4e01f456
Aawaj Tamang <ataavash@gmail.com>
2026-02-22 18:46:52 +0545

    Add files via upload

---
+++ passfail.java
@@ -0,0 +1,13 @@
+package OOPjava.sem1.lab3;
+
+public class passfail {
+    public static void main(String[] arg){
+        int Marks = 55;
+        if (Marks >= 40){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
