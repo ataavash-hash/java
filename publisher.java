684425c9068dffe3f50d032c7ce4c522bf0d3d6b
Aawaj Tamang <ataavash@gmail.com>
2026-03-02 22:50:13 +0545

    Add files via upload

---
+++ publisher.java
@@ -0,0 +1,15 @@
+package practice.java;
+
+public class publisher {
+    String title;
+    Double price;
+    static final String name = "aawaj";
+
+    static void main(String[] args) {
+    }
+    void publisher(){
+        System.out.println("the name of the book auther is  Aawaj ");
+        System.out.println("title is "+title);
+        System.out.println("price is "+price);
+    }
+}
