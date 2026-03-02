196ad1687af0704c2d79c41afd7fc693d0c1acce
Aawaj Tamang <ataavash@gmail.com>
2026-03-02 22:50:29 +0545

    Add files via upload

---
+++ publisher2.java
@@ -0,0 +1,10 @@
+package practice.java;
+
+public class publisher2 {
+    void main(){
+        publisher b1 = new publisher();
+        b1.title= "qwerty";
+        b1.price= 123.0;
+        b1.publisher();
+    }
+}
